d18766f0be5dbafd5af48d09945866becf0f071e
v.nichnikov <57954863+VasiliyNichnikov@users.noreply.github.com>
2023-04-29 23:12:52 +0300

    Добавлена работа камеры + внесены правки в скрипт движения игрока

--- Assets/Scripts/Core/Payer/PlayerMove.cs
+++ Assets/Scripts/Core/Payer/PlayerMove.cs
@@ -1,46 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+namespace Core.Payer
 {
-    public float MoveSpeed;
-    private float _corentSpeed;
-    private float _x_Move;
-    private float _z_Move;
-    private CharacterController _player;
-    private Vector3 _move_Derection;
-
-    void Start()
+    public class PlayerMove : MonoBehaviour
     {
-        _player = GetComponent<CharacterController>();    
-    }
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _runSpeed;
 
-    void Update()
-    {
-        Move();    
-    }
+        private float _xMove;
+        private float _zMove;
+        private CharacterController _player;
+        private Vector3 _moveDirection;
 
-    void Move()
-    {
-        _x_Move = Input.GetAxis("Horizontal");
-        _z_Move = Input.GetAxis("Vertical");
-        if (_player.isGrounded)
+        private void Start()
         {
-            _move_Derection = new Vector3(_x_Move, 0f,_z_Move);
-            _move_Derection = transform.TransformDirection(_move_Derection);
+            _player = GetComponent<CharacterController>();
         }
 
-        _move_Derection.y = -1;
+        private void Update()
+        {
+            Move();
+        }
 
-        if ( Input.GetKey(KeyCode.LeftShift))
+        private void Move()
         {
-            _corentSpeed = MoveSpeed * 2;
+            _xMove = Input.GetAxis("Horizontal");
+            _zMove = Input.GetAxis("Vertical");
+            if (_player.isGrounded)
+            {
+                _moveDirection = new Vector3(_xMove, 0f, _zMove);
+                _moveDirection = transform.TransformDirection(_moveDirection);
+            }
+
+            _moveDirection.y = -1;
+
+            var currentSpeed = GetCurrentSpeed();
+            _player.Move(_moveDirection * currentSpeed * Time.deltaTime);
         }
-        else
+
+        private float GetCurrentSpeed()
         {
-            _corentSpeed = MoveSpeed;
+            return Input.GetKey(KeyCode.LeftShift) ? _moveSpeed : _runSpeed;
         }
-        _player.Move(_move_Derection * _corentSpeed * Time.deltaTime);
     }
 }
