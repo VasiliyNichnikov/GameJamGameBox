46f9be5882d8e9dd4ff92494c4f8ea27db31b21c
GliZoW <78784567+GliZoW@users.noreply.github.com>
2023-04-29 19:43:58 +0300

    Управление персонажем на WASD

---
+++ Assets/Scripts/Core/Payer/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float Speed = 7;
+    private float _x_Move;
+    private float _z_Move;
+    private CharacterController _player;
+    private Vector3 _move_Derection;
+
+    void Start()
+    {
+        _player = GetComponent<CharacterController>();    
+    }
+
+    void Update()
+    {
+        Move();    
+    }
+
+    void Move()
+    {
+        _x_Move = Input.GetAxis("Horizontal");
+        _z_Move = Input.GetAxis("Vertical");
+        if (_player.isGrounded)
+        {
+            _move_Derection = new Vector3(_x_Move, 0f,_z_Move);
+            _move_Derection = transform.TransformDirection(_move_Derection);
+        }
+        _move_Derection.y = -1;
+        _player.Move(_move_Derection * Speed * Time.deltaTime);
+    }
+}
