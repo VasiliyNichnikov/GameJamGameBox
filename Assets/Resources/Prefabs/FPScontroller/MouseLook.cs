01f7860754800e99f2d5e9528159c02afc74077f
GliZoW <78784567+GliZoW@users.noreply.github.com>
2023-05-02 20:12:26 +0300

    Добавил ограничения вращения камеры по Y

--- Assets/Resources/Prefabs/FPScontroller/MouseLook.cs
+++ Assets/Resources/Prefabs/FPScontroller/MouseLook.cs
@@ -6,6 +6,8 @@ public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float _mouseSensitivity = 100f;
     [SerializeField] private Transform controller;
+    [SerializeField] float _upY;
+    [SerializeField] float _downY;
     float _xRotation = 0f;
 
     void Start()
@@ -20,7 +22,8 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
 
         _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        _xRotation = Mathf.Clamp(_xRotation, -_upY, _downY);
+
 
         transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
 
